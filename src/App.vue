f9685ca5b0a390915d6f011802dbaa73f5b3ed36
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2022-10-15 17:14:09 -0300

    Feat: Creating part of html and css base

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
+
+    <MyHeader />
+
   </div>
 </template>
 
 <script>
+import '../src/styles/global.css';
+
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    MyHeader
   }
 }
 </script>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div className="container">
+        <div className="purchase-order"></div>
+        <div className="store-name"></div>
+        <div className="value"></div>
+        <div className="suplier"></div>
+        <div className="ship-to"></div>
+        <div className="bill-to"></div>
+        <div className="charge-to"></div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'MyHeader'
+}
+</script>
