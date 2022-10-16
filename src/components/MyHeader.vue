b0970450a9acc0d40c66e23d2d65df247c176be6
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2022-10-16 10:54:45 -0300

    Feat: Somo modifications in MyHeader and css || Configuring axios

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,12 +1,30 @@
 <template>
     <div className="container">
-        <div className="purchase-order"></div>
-        <div className="store-name"></div>
-        <div className="value"></div>
-        <div className="suplier"></div>
-        <div className="ship-to"></div>
-        <div className="bill-to"></div>
-        <div className="charge-to"></div>
+        <div className="purchase-order">
+            <div>
+                <h2>Purchase Order</h2>
+                <p>{order.header.number}</p>
+                <p>SerialMe <span>{order.header.serial}</span></p>
+            </div>
+        </div>
+        <div className="store-name">
+            
+        </div>
+        <div className="value">
+            
+        </div>
+        <div className="suplier">
+
+        </div>
+        <div className="ship-to">
+            <h2 class="subtitle-footer"></h2>
+        </div>
+        <div className="bill-to">
+            <h2 class="subtitle-footer"></h2>
+        </div>
+        <div className="charge-to">
+            <h2 class="subtitle-footer"></h2>
+        </div>
     </div>
 </template>
 
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL:'https://me-frontend-challenge-api.herokuapp.com/orders/1'
+})
+
+export default api
